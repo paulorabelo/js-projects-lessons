6606eade73a6a673d95cb80da4848730778b6f6e
Paulo Rabelo <paulorabelo@hotmail.com>
2021-05-12 23:15:38 -0300

    Adicionado primeiro Sprite no canvas.

--- flappy-bird/jogo.js
+++ flappy-bird/jogo.js
@@ -1,8 +1,35 @@
-console.log('[DevSoutinho] Flappy Bird');
-console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
+console.log('[PauloRabeloDev] Flappy Bird');
 
 const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 23,
+    x: 10,
+    y: 50,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, // Posição do Sprite x, y
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na Sprite
+            flappyBird.x, flappyBird.y, // Posição no Canvas
+            flappyBird.largura, flappyBird.altura, // Tamanho no Canvas
+            );
+
+    }
+}
+
+
+function loop() {
+        flappyBird.desenha();    
+    
+        requestAnimationFrame(loop);
+    }
+    
+    loop();
