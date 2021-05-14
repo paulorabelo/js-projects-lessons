7f2bec907473c30081186f4e292168d1b9290445
Paulo Rabelo <paulorabelo@hotmail.com>
2021-05-13 23:00:58 -0300

    alterado arquivo jogo.js adicionado fundo, cor e paisagem

--- flappy-bird/jogo.js
+++ flappy-bird/jogo.js
@@ -6,11 +6,72 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
+// [Plano de Fundo]
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 276,
+    altura: 204,
+    x: 0,
+    y: canvas.height-204,
+    desenha(){
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+            );
+
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+            );
+
+    }
+}
+
+
+// [chao]
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height -112,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+            );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura,
+            );
+
+    }
+}
+
+// [flappyBird]
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
-    largura: 33,
-    altura: 23,
+    largura: 34,
+    altura: 24,
     x: 10,
     y: 50,
     desenha(){
@@ -27,9 +88,14 @@ const flappyBird = {
 
 
 function loop() {
-        flappyBird.desenha();    
+
+    planoDeFundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
     
-        requestAnimationFrame(loop);
+    // flappyBird.y = flappyBird.y + 1;
+
+    requestAnimationFrame(loop);
     }
     
     loop();
